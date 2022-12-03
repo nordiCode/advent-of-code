21487651744f35c3a06c4a2dcd716fa1b9a3a114
nordiCode.github.io <grantsbfi@gmail.com>
2022-12-02 23:38:16 -0800

    day one part 1

--- Advent-2021/day_one.hs
+++ Advent-2021/day_one.hs
@@ -1,15 +1,15 @@
 module Main where
 
-
--- cleanContents :: String -> [String]
--- cleanContents =  map read . unlines .words
-
-
 readInt :: String -> Int
 readInt =  read
+
+mapInc :: (Int, Int) -> Int
+mapInc (x,y)
+  | x < y = 1
+  | otherwise = 0
 
 main :: IO ()
 main = do
     contents <- readFile "day_one_input.txt" 
-    let intArray = map readInt . lines $ contents
-    print $ intArray
+    let xs = map readInt . lines $ contents
+    print $ sum . map mapInc $ zip <*> tail $ xs
