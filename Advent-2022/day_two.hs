664c09782dbcef64645e5695d2fc354d65f53dcc
nordiCode.github.io <grantsbfi@gmail.com>
2022-12-02 21:00:32 -0800

    Adding Types

--- Advent-2022/day_two.hs
+++ Advent-2022/day_two.hs
@@ -1,51 +1,51 @@
--- Extremely naive lunchhour solution !
+data Piece = Rock | Paper | Scissors
+  deriving (Show, Eq, Enum)
 
-choiceScore :: (a, Char) -> Int
-choiceScore (_,x) 
-  | x == 'X' = 1
-  | x == 'Y' = 2
-  | x == 'Z' = 3
+outCome :: (Piece, Piece) -> Int
+outCome (x, y)
+  | y == Rock && x == Scissors || y == Paper && x == Rock || y == Scissors && x == Paper = 6
+  | x == y = 3
   | otherwise = 0
 
-outCome :: (Char,Char) -> Int
-outCome (x,y) 
- | y == 'X' && x == 'C' || y == 'Y' && x == 'A' || y == 'Z' && x == 'B' = 6
- | y == 'X' && x == 'A' || y == 'Z' && x == 'C' || y == 'Y' && x == 'B' = 3
- | otherwise = 0
+changeSecond :: (Piece, Piece) -> (Piece, Piece)
+changeSecond (m, n)
+  | n == Scissors = swapWin (m, n)
+  | n == Paper = swapDraw (m, n)
+  | n == Rock = swapLose (m, n)
 
-changeSecond :: (Char,Char) -> (Char, Char)
-changeSecond (m,n)
-  | n == 'Z' = swapWin (m,n)
-  | n == 'Y' = swapDraw (m,n)
-  | n == 'X' = swapLose (m,n)
+swapWin :: (Piece, Piece) -> (Piece, Piece)
+swapWin (m, _)
+  | m == Rock = (m, Paper)
+  | m == Paper = (m, Scissors)
+  | otherwise = (m, Rock)
 
-swapWin :: (Char,Char) -> (Char,Char)
-swapWin (m,_)
-  | m == 'A' = (m,'Y')
-  | m == 'B' = (m,'Z')
-  | otherwise = (m,'X')
+swapDraw :: (Piece, Piece) -> (Piece, Piece)
+swapDraw (m, _) = (m, m)
 
-swapDraw :: (Char,Char) -> (Char,Char)
-swapDraw (m,_)
-  | m == 'A' = (m,'X')
-  | m == 'B' = (m,'Y')
-  | otherwise = (m,'Z')
+swapLose :: (Piece, Piece) -> (Piece, Piece)
+swapLose (m, _)
+  | m == Rock = (m, Scissors)
+  | m == Paper = (m, Rock)
+  | otherwise = (m, Paper)
 
-
-swapLose :: (Char,Char) -> (Char,Char)
-swapLose (m,_)
-  | m == 'A' = (m,'Z')
-  | m == 'B' = (m,'X')
-  | otherwise = (m,'Y')
-
-sumResult :: (Char, Char) -> Int
-sumResult x = choiceScore x + outCome x
+sumResult :: (Piece, Piece) -> Int
+sumResult (x, y) = fromEnum y + 1 + outCome (x, y)
 
 -- need to not use Head/Tail
 mapTuple :: [b] -> (b, b)
 mapTuple x = (head x, last x)
 
+tuplePiece :: (Char, Char) -> (Piece, Piece)
+tuplePiece (m, n) = (mapPiece m, mapPiece n)
+
+mapPiece :: Char -> Piece
+mapPiece x
+  | x == 'A' || x == 'X' = Rock
+  | x == 'B' || x == 'Y' = Paper
+  | x == 'C' || x == 'Z' = Scissors
+
 main :: IO ()
 main = do
   contents <- readFile "day_two.txt"
-  print $ sum . map sumResult $ map changeSecond $ map mapTuple $ lines contents 
+  -- print $ sum . map sumResult $ map changeSecond $ map mapTuple $ lines contents
+  print $ sum . map (sumResult . tuplePiece . mapTuple) $ lines contents
