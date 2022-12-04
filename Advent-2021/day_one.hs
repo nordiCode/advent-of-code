d41e78ea90cc36abc346dd3f8fd4e349971f9278
nordiCode.github.io <grantsbfi@gmail.com>
2022-12-04 13:19:04 -0800

    2021 day one

--- Advent-2021/day_one.hs
+++ Advent-2021/day_one.hs
@@ -1,5 +1,8 @@
 module Main where
 
+import Data.List.Split
+import Data.List
+
 readInt :: String -> Int
 readInt =  read
 
@@ -8,8 +11,12 @@ mapInc (x,y)
   | x < y = 1
   | otherwise = 0
 
+windows n xs = transpose (take n (tails xs))
+-- print $ sum . map mapInc $ zip <*> tail $ xs
+
 main :: IO ()
 main = do
     contents <- readFile "day_one_input.txt" 
     let xs = map readInt . lines $ contents
-    print $ sum . map mapInc $ zip <*> tail $ xs
+    print  $ sum . map mapInc $ zip <*> tail $ map sum $ windows 3 xs
+    
