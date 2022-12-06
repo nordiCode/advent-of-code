613d61fe0b2afecebd5b463d7a177c760d6113d7
nordiCode.github.io <grantsbfi@gmail.com>
2022-12-05 21:47:48 -0800

    day six

---
+++ Advent-2022/day_five.hs
@@ -0,0 +1,15 @@
+import Data.List.Split (splitOn)
+import Data.Char
+
+
+
+
+main :: IO ()
+main = do
+  contents <- readFile "./res/day_five.txt"
+  let x = lines contents
+  let s = "yo"
+  print $ s
+
+
+ 
