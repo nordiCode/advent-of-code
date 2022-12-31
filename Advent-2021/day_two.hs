966ad555275f1bfa2fb44b335e74cb4471ea2391
nordiCode.github.io <grantsbfi@gmail.com>
2022-12-31 09:39:06 -0800

    day two 2021

--- Advent-2021/day_two.hs
+++ Advent-2021/day_two.hs
@@ -1,12 +1,31 @@
 import Data.List.Split
 
-readInt :: [String, String] -> [String,Int]
-readInt = map read
 
-intList [x,y] = [x,read y]
+data Position = Up | Down | Forward
+ deriving (Show,Eq)
+
+
+intTuple :: [String] -> (String, Int)
+intTuple xs =  (xs !! 0, read (xs !! 1)::Int)
+
+
+incPosition :: [[String]] -> Int -> Int -> Int
+incPosition [] n h = n * (abs h)
+incPosition (x:xs) n h
+  | head x == "forward" = incPosition xs (n+(read (last x)::Int)) h
+  | head x == "down" = incPosition xs n (h - (read (last x)::Int)) 
+  | head x == "up" = incPosition xs n (h + (read (last x)::Int)) 
+  | otherwise = incPosition xs n h
+
+mapPosition :: String -> Position 
+mapPosition x 
+  | x == "forward" = Forward
+  | x == "up" = Up
+  | x == "down" = Down
 
 
 main :: IO ()
 main = do
   contents <- readFile "day_two.txt"
-  print $ map (intList . splitOn " ") $ lines contents
+  let array = map (splitOn " ") . lines $ contents
+  print $ incPosition array 0 0
